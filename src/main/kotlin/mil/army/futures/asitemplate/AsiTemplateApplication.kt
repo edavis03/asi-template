0610af7fc19042cd6e0dc182a208655992d57404
Omeed Safi <osafi@vmware.com>
2022-01-25 13:40:38 -0600

    Convert to Kotlin

---
+++ src/main/kotlin/mil/army/futures/asitemplate/AsiTemplateApplication.kt
@@ -0,0 +1,11 @@
+package mil.army.futures.asitemplate
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AsiTemplateApplication
+
+fun main(args: Array<String>) {
+    runApplication<AsiTemplateApplication>(*args)
+}
